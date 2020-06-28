5755bb589f626579a776c92e6b1f093212f61162
Hongri Cui <choihongil@gmail.com>
2020-06-28 21:12:53 +0900

    remove roles except media_server

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,64 +1,27 @@
 #!/bin/bash
 set -eu
 
-case $(uname) in
-    Linux)
-        # python3-distutils for pip install
-        # gnupg for add apt key
-        echo "Install pre-required packages"
-        sudo apt install --no-install-recommends --yes \
-                git \
-                gnupg \
-                python3-distutils
-        sudo apt upgrade
-        # pip
-        if [ ! -x ~/.local/bin/pip3 ]; then
-            echo "Install pip"
-            wget https://bootstrap.pypa.io/get-pip.py
-            python3 get-pip.py --user
-        fi
-        # ansible
-        if [ ! -x ~/.local/bin/ansible ]; then
-            echo "Install ansible"
-            ~/.local/bin/pip3 install --user ansible
-        fi
-        ANSIBLE_PLAYBOOK="${HOME}/.local/bin/ansible-playbook"
-        
-        # select machine type
-        PS3="Select machine type: "
-        options=("notebook" "media server")
-        select opt in "${options[@]}"
-        do
-            case $opt in
-                notebook)
-                    PLAYBOOK="notebook-playbook.yml"
-                    break
-                    ;;
-                "media server")
-                    PLAYBOOK="media_server-playbook.yml"
-                    break
-                    ;;
-                *)
-                    echo "Invalid option"
-                    ;;
-            esac
-        done
-        ;;
-    Darwin)
-	# brew
-	if ! which brew > /dev/null; then
-	    echo "Install brew"
-	    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	fi
-	# ansible
-	if ! brew list | grep ansible > /dev/null; then
-	    echo "Install ansible"
-	    brew install ansible
-	fi
-        ANSIBLE_PLAYBOOK=/usr/local/bin/ansible-playbook
-        PLAYBOOK="mac-playbook.yml"
-        ;;
-esac
+# python3-distutils for pip install
+# gnupg for add apt key
+echo "Install pre-required packages"
+sudo apt install --no-install-recommends --yes \
+        git \
+        gnupg \
+        python3-distutils
+sudo apt upgrade
+# pip
+if [ ! -x ~/.local/bin/pip3 ]; then
+    echo "Install pip"
+    wget https://bootstrap.pypa.io/get-pip.py
+    python3 get-pip.py --user
+fi
+# ansible
+if [ ! -x ~/.local/bin/ansible ]; then
+    echo "Install ansible"
+    ~/.local/bin/pip3 install --user ansible
+fi
+ANSIBLE_PLAYBOOK="${HOME}/.local/bin/ansible-playbook"
+PLAYBOOK="media_server-playbook.yml"
 
 # debian playbook
 if [ ! -d ~/debian-playbook ]; then
